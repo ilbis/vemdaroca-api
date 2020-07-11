af222acfc8e8f3fc9028c2148766a2a08c1521c2
Jenkins CI-CD <devops@santander.com.br>
2020-07-11 14:16:57 -0300

    Initial Commit

---
+++ src/main/java/com/vemdaroca/vemdarocaapi/VemdarocaApiApplication.java
@@ -0,0 +1,13 @@
+package com.vemdaroca.vemdarocaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VemdarocaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VemdarocaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vemdaroca/vemdarocaapi/VemdarocaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vemdaroca.vemdarocaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VemdarocaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
