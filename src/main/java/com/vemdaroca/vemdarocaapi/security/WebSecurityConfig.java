536f9abb23bd854f602318220a8400efa3546cbe
ilbis <ilbis.candido@gmail.com>
2020-10-26 23:08:31 -0300

    Add * cors

--- src/main/java/com/vemdaroca/vemdarocaapi/security/WebSecurityConfig.java
+++ src/main/java/com/vemdaroca/vemdarocaapi/security/WebSecurityConfig.java
@@ -82,7 +82,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("OPTIONS","GET","POST"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
